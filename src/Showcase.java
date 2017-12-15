793dc1e18535f3da1c8a7d0067cfd5af1ca2f302
Harsh Shandilya <harsh@prjkt.io>
2017-12-15 18:51:11 +0530

    database: Cleanup showcase updater
    * Remove unneeded .class files and use spaces
      in places of tabs in src/
    
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- src/Showcase.java
+++ src/Showcase.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 public class Showcase {
 
     public static void main(String[] args) {
-    	System.out.println("=========================================================================================");
-    	System.out.println("         Welcome to [projekt.]'s automatic Substratum Showcase cloud updater!");
-    	System.out.println("=========================================================================================");
+        System.out.println("=========================================================================================");
+        System.out.println("         Welcome to [projekt.]'s automatic Substratum Showcase cloud updater!");
+        System.out.println("=========================================================================================");
 
-    	ArrayList<String> files = new ArrayList<>();
+        ArrayList<String> files = new ArrayList<>();
 
         File folder = new File(System.getProperty("user.dir"));
         System.out.println("\nLocating XML files in directory : " + folder.getAbsolutePath() + "\n");
@@ -16,21 +16,19 @@ public class Showcase {
 
         for (int i = 0; i < listOfFiles.length; i++) {
           if (listOfFiles[i].isFile() && listOfFiles[i].toString().endsWith(".xml")) {
-        	  files.add(listOfFiles[i].getName());
-        	  System.out.println("Found XML file : " + listOfFiles[i].getName());
+              files.add(listOfFiles[i].getName());
+              System.out.println("Found XML file : " + listOfFiles[i].getName());
           }
         }
-        
         for (int i = 0; i < files.size(); i++) {
-        	System.out.println("\n============================================"
-        			+ "=============================================");
-        	System.out.println("Processing file : " + files.get(i));
-        	System.out.println("============================================"
-        			+ "=============================================\n");
-        	String[] checkerCommands = {files.get(i), "true"};
+            System.out.println("\n============================================"
+                    + "=============================================");
+            System.out.println("Processing file : " + files.get(i));
+            System.out.println("============================================"
+                    + "=============================================\n");
+            String[] checkerCommands = {files.get(i), "true"};
             RefactorFile.main(checkerCommands);
         }
-        
-    	System.out.println("\nThe program has finished.");
+        System.out.println("\nThe program has finished.");
     }
 }
